77ed68a9bbdd34ece9b1f7932f39ca3f212ece1b
junwon_lee <wjdrbs307@naver.com>
2023-12-21 12:53:34 +0900

    Revert "밥먹으러가기전"
    This reverts commit 4ae8937a0c214881a95cf18c10fdb8ef5d6fedcd.

--- src/App.js
+++ src/App.js
@@ -9,35 +9,62 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import data from './data.js'
 import { useState, useEffect } from 'react';
-import { Routers, Route, Link, Routes, useNavigate, Outlet } from 'react-router-dom'
+import { Routers, Route, Link, Routes, useNavigate, Outlet  } from 'react-router-dom'
 import Message from './\bcomponent/message';
-import Header from './\bcomponent/header';
-import Footer from './\bcomponent/footer';
-import Main from './\bcomponent/main';
 
 
 function App() {
 
+  const [intro] = useState(data);
   const navigaet = useNavigate();
 
   return (
     <div className="App">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand onClick={()=>{ navigaet('/') }}>Stx Network Service</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link onClick={()=>{ navigaet('/') }}></Nav.Link>
+              <Nav.Link onClick={()=>{ navigaet('/message') }}>인사말</Nav.Link>
+              <Nav.Link href="/info" element={<Message/>}>사업소개</Nav.Link>
+              <Nav.Link href="/estimate">견적문의</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <Header></Header>
+      {/* <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link> */}
+
       <Routes>
-        <Route path='/' Component={Main} />
+        <Route path='/' element={
+          <div>
+            <div className='main-bg' style={{ backgroundImage: 'url(' + bg + ')' }}> </div>
+            <Container className='sub-bg'>
+              <Row>
+                {
+                  intro.map((a, i) => {
+                    return <SubCard intro={intro[i]} i={i + 2}></SubCard>
+                  })
+                }
+              </Row>
+            </Container>
+          </div>
+        } />
+        <Route path='/message' Component={Message}/>
+        
+        <Route path='/info' element={<Info/>}>  {/* nested routes */}
+          <Route path='member' element={<div>회사 조직도</div>}/> {/* path='/info/member' */}
+          <Route path='location' element={<div>회사 위치정보</div>}/> 
+        </Route>
 
-        {/* 회사소개 */}
-        <Route path='/message' Component={Message} />
-
-         {/* 사업분야 */}
         <Route path='/estimate' element={<div>견적문의</div>} />
         <Route path='*' element={<div>404페이지</div>}></Route>
-
-         {/* 고객지원 */}
       </Routes>
 
-      <Footer></Footer>
+
 
     </div>
   );
@@ -46,3 +73,21 @@ function App() {
 export default App;
 
 
+function SubCard(props) {
+  return (
+    <Col sm>
+      <div className={'tech' + props.i}></div>
+      <h4>{props.intro.title}</h4>
+      <p>{props.intro.content}</p>
+    </Col>
+  )
+} 
+
+function Info() {
+  return (
+    <div>
+      <h4>사업소개</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
