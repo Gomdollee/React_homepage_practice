0446810146d039dec83a6de73113aeaaf1104627
junwon_lee <wjdrbs307@naver.com>
2023-12-21 13:25:06 +0900

    1

--- src/component/main.js
+++ src/component/main.js
@@ -19,9 +19,8 @@ function Card() {
         <Container className='main-card'>
             <Row>
                 <Col>
-                    <div className='tech2'>인사말     </div>
-                        <span className='tech2_sp'>Ceo Message</span>dd
-               
+                    <div className='tech2'>인사말 </div>
+                        <span className='tech2_sp'>Ceo Message</span>
                 </Col>
                 <Col>
                     <div className='tech3'>사업소개</div>
